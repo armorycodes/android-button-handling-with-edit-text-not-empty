62d47e9dcfdd141b4e9a5c155789e427196b65e6
amirisback <faisalamircs@gmail.com>
2022-08-31 21:23:14 +0700

    validate the button

--- app/src/main/java/com/frogobox/research/MainActivity.kt
+++ app/src/main/java/com/frogobox/research/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.frogobox.research
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
+import com.frogobox.research.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initView()
     }
+
+    private fun initView() {
+        binding.apply {
+
+            et1.addTextChangedListener(textWatcher)
+            et2.addTextChangedListener(textWatcher)
+            et3.addTextChangedListener(textWatcher)
+            et4.addTextChangedListener(textWatcher)
+            et5.addTextChangedListener(textWatcher)
+
+        }
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            binding.button.isEnabled = validateButton()
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            binding.button.isEnabled = validateButton()
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            binding.button.isEnabled = validateButton()
+        }
+    }
+
+    private fun validateButton(): Boolean {
+        binding.apply {
+            return et1.validateText() &&
+                    et2.validateText() &&
+                    et3.validateText() &&
+                    et4.validateText() &&
+                    et5.validateText()
+        }
+    }
+
+    private fun AppCompatEditText.validateText(): Boolean {
+        return text.toString().isNotEmpty()
+    }
+
+
 }
